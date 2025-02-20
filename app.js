fba40d691097ed651c58055a7960a00152e6d106
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-02-20 22:51:19 +0500

    Add public directory for assets

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const path = require("node:path");
 const express = require("express");
 const app = express();
 
+const assetsPath = path.join(__dirname, "public");
+
 require("dotenv").config();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(express.static(assetsPath));
 
 const PORT = process.env.PORT;
 
