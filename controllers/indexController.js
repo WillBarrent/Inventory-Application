89356cba5ad3ece072f6a8d21a1884a7f52a12dc
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-02-23 15:44:05 +0500

    Pass all selected pokemons and its column names from db to the view

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,9 +1,16 @@
+const { getAllPokemons } = require("../db/db");
+
 async function indexGet(req, res) {
+  const pokemons = await getAllPokemons();
+  const keys = Object.keys(pokemons[0]);
+
   res.render("index", {
-    title: "Inventory App",
+    title: "All Pokemons",
+    pokemons: pokemons,
+    keys: keys,
   });
 }
 
 module.exports = {
-    indexGet,
+  indexGet,
 };
