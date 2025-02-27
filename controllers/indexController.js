af34ebc6e40e700e189ee798a922d9b35375e199
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-02-27 22:47:23 +0500

    Handle post request to /create

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,10 @@
-const { body } = require("express-validator");
-const { getAllPokemons, getAllTrainers, getAllTypes } = require("../db/db");
+const { body, validationResult } = require("express-validator");
+const {
+  getAllPokemons,
+  getAllTrainers,
+  getAllTypes,
+  addPokemon,
+} = require("../db/db");
 
 const validatePokemon = [
   body("pokemon_name")
@@ -28,12 +33,49 @@ async function indexCreateGet(req, res) {
     title: "Create a new Pokemon",
     trainers: trainers,
     types: types,
+    errors: [],
   });
 }
 
 const indexCreatePost = [
   validatePokemon,
   async function indexCreatePost(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const trainers = await getAllTrainers();
+      const types = await getAllTypes();
+
+      return res.status(400).render("pokemons/create", {
+        title: "Create a new Pokemon",
+        trainers: trainers,
+        types: types,
+        errors: errors.array(),
+      });
+    }
+
+    const body = req.body;
+    const pokemonName = body["pokemon_name"];
+    const trainerId = body["trainer"];
+    const typeId = body["type"];
+
+    const result = await addPokemon({ pokemonName, trainerId, typeId });
+
+    if (result === false) {
+      const trainers = await getAllTrainers();
+      const types = await getAllTypes();
+
+      return res.render("pokemons/create", {
+        title: "Create a new Pokemon",
+        trainers: trainers,
+        types: types,
+        errors: [
+          {
+            msg: "It seems you can't create pokemon with these combinations of data",
+          },
+        ],
+      });
+    }
+
     res.redirect("/");
   },
 ];
