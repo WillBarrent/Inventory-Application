e2d45e65c9d565f15858f55c7362a0075888a98a
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-03-09 15:37:24 +0500

    Add db methods for types

--- db/db.js
+++ db/db.js
@@ -178,6 +178,79 @@ async function deleteTrainer(id) {
   await pool.query("DELETE FROM pokemons WHERE id = ANY ($1)", [idsOfPokemons]);
 }
 
+async function getAllTypesPokemons(typeId) {
+  const { rows } = await pool.query(
+    `
+    SELECT pokemons.id, pokemon_name, trainer, type_name FROM pokemons
+    INNER JOIN trainers
+    ON trainers.id = pokemons.pokemon_trainer_id
+    INNER JOIN types
+    ON types.id = pokemon_type_id
+    WHERE pokemon_type_id = $1
+    ORDER BY id;
+  `,
+    [typeId]
+  );
+
+  return rows;
+}
+
+async function addType(typeName) {
+  const { rows: types } = await pool.query("SELECT * FROM types");
+
+  const typeExist = types.find((type) => {
+    return type["type_name"] == typeName;
+  });
+
+  if (typeExist) {
+    return false;
+  }
+
+  await pool.query("INSERT INTO types (type) VALUES ($1)", [typeName]);
+}
+
+async function updateType(id, typeName) {
+  const { rows: types } = await pool.query("SELECT * FROM types");
+
+  const typeExist = types.find((type) => {
+    return type["type_name"] == typeName;
+  });
+
+  if (typeExist) {
+    return false;
+  }
+
+  await pool.query("UPDATE types SET type = $1 WHERE id = $2", [
+    typeName,
+    id,
+  ]);
+}
+
+async function deleteType(id) {
+  const { rows: types } = await pool.query(
+    "SELECT * FROM types WHERE id = $1",
+    [id]
+  );
+
+  if (!types) {
+    return false;
+  }
+
+  await pool.query("DELETE FROM types WHERE id = $1", [id]);
+
+  const { rows: pokemonsToDelete } = await pool.query(
+    `
+    SELECT * FROM pokemons WHERE pokemon_type_id = $1`,
+    [id]
+  );
+
+  const idsOfPokemons = pokemonsToDelete.map((pokemon) => {
+    return pokemon["id"];
+  });
+
+  await pool.query("DELETE FROM pokemons WHERE id = ANY ($1)", [idsOfPokemons]);
+}
+
 module.exports = {
   getAllPokemons,
   getPokemonById,
@@ -191,4 +264,8 @@ module.exports = {
   addTrainer,
   updateTrainer,
   deleteTrainer,
+  getAllTypesPokemons,
+  addType,
+  updateType,
+  deleteType,
 };
